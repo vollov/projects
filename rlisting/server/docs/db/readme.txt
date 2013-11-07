d59bedfaf703b20466f717fe28af843f8b451aba
dike.zhang <dike.zhang@gmail.com>
2013-11-06 20:14:44 -0500

    add schema docs

---
+++ rlisting/server/docs/db/readme.txt
@@ -0,0 +1,25 @@
+,
+,AB
+,BC
+,MB
+,NB
+,NL
+,NS
+,ON
+,PE
+,QC
+,SK
+
+total numbers
+
+postcode = 888688
+city = 8271
+
+[one-to-many]
+city -- post code 
+role -- user
+
+landlord -- property
+
+many-to-many
+role -- action
--- rlisting/server/schema.js
+++ rlisting/server/schema.js
@@ -6,3 +6,13 @@ users = [{password: '30274c47903bd1bac7633bbf09743149ebab805f', is_active: false
          {password: '5bf1fd927dfb8679496a2e6cf00cbe50c1c87145', is_active: true, email: 'fred@gmail.ca',role:2}];
 db.user.insert(users);
 db.user.ensureIndex({email: 1}, {unique: true});
+
+db.role.drop();
+roles = [{name:'admin', id:1}, {name:'member', id:2}, {name:'guest', id:3}];
+db.role.insert(roles);
+db.role.ensureIndex({name: 1}, {unique: true});
+
+db.route.drop();
+routes = [{path:'/home', roles: [3]}, {path:'/about', roles: [3]}, {path:'/users', roles: [1]}];
+db.route.insert(routes);
+db.route.ensureIndex({path: 1}, {unique: true});
